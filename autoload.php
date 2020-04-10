448e4cb195a432391ee0ac0b36aee27cc24ba0cd
Marien Fressinaud <dev@marienfressinaud.fr>
2020-04-10 11:54:50 +0200

    tec: Provide FPDF lib

--- autoload.php
+++ autoload.php
@@ -7,11 +7,13 @@ spl_autoload_register(function ($class_name) {
 
     if (strpos($class_name, 'Minz') === 0) {
         include($lib_path . '/Minz/autoload.php');
-    } elseif (strpos($class_name, 'Stripe') === 0) {
-        include($lib_path . '/stripe-php/init.php');
     } elseif (strpos($class_name, $app_name) === 0) {
         $class_name = substr($class_name, strlen($app_name) + 1);
         $class_path = str_replace('\\', '/', $class_name) . '.php';
         include($app_path . '/src/' . $class_path);
+    } elseif (strpos($class_name, 'Stripe') === 0) {
+        include($lib_path . '/stripe-php/init.php');
+    } elseif ($class_name === 'FPDF') {
+        include($lib_path . '/fpdf/fpdf.php');
     }
 });
