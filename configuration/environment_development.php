122bbc5b55992a982b16f381655ccf0713b8f44b
Marien Fressinaud <dev@marienfressinaud.fr>
2020-04-10 14:33:13 +0200

    tec: Provide a flus_private_key in dev env

--- configuration/environment_development.php
+++ configuration/environment_development.php
@@ -10,6 +10,8 @@ return [
         'stripe_private_key' => getenv('APP_STRIPE_PRIVATE_KEY'),
         'stripe_public_key' => getenv('APP_STRIPE_PUBLIC_KEY'),
         'stripe_webhook_secret' => getenv('APP_STRIPE_WEBHOOK_SECRET'),
+
+        'flus_private_key' => getenv('FLUS_PRIVATE_KEY'),
     ],
     'database' => [
         'dsn' => "sqlite:{$app_path}/data/db.sqlite",
