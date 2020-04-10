5c951a65e769c7e3f1ebef8528343d06429dc393
Marien Fressinaud <dev@marienfressinaud.fr>
2020-04-10 17:53:06 +0200

    tec: Allow to setup production environment

---
+++ configuration/environment_production.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'app_name' => 'Website',
+    'url_options' => [
+        'protocol' => 'https',
+        'host' => getenv('APP_HOST'),
+    ],
+    'application' => [
+        'stripe_private_key' => getenv('APP_STRIPE_PRIVATE_KEY'),
+        'stripe_public_key' => getenv('APP_STRIPE_PUBLIC_KEY'),
+        'stripe_webhook_secret' => getenv('APP_STRIPE_WEBHOOK_SECRET'),
+
+        'flus_private_key' => getenv('FLUS_PRIVATE_KEY'),
+    ],
+    'database' => [
+        'dsn' => "sqlite:{$app_path}/data/db.sqlite",
+    ],
+];
