19c44e4d6923f010b88e83b707a56e7ca13c4604
Marien Fressinaud <dev@marienfressinaud.fr>
2020-04-27 17:44:21 +0200

    tec: Always load configuration from a .env file

--- configuration/environment_production.php
+++ configuration/environment_production.php
@@ -1,23 +1,25 @@
 <?php
 
+$dotenv = new \Minz\Dotenv($app_path . '/.env');
+
 return [
     'app_name' => 'Website',
 
     'url_options' => [
         'protocol' => 'https',
-        'host' => getenv('APP_HOST'),
+        'host' => $dotenv->pop('APP_HOST'),
     ],
 
     'application' => [
-        'enabled' => getenv('APP_ENABLED'),
+        'enabled' => $dotenv->pop('APP_ENABLED'),
 
-        'admin_secret' => getenv('APP_ADMIN_SECRET'),
+        'admin_secret' => $dotenv->pop('APP_ADMIN_SECRET'),
 
-        'stripe_private_key' => getenv('APP_STRIPE_PRIVATE_KEY'),
-        'stripe_public_key' => getenv('APP_STRIPE_PUBLIC_KEY'),
-        'stripe_webhook_secret' => getenv('APP_STRIPE_WEBHOOK_SECRET'),
+        'stripe_private_key' => $dotenv->pop('APP_STRIPE_PRIVATE_KEY'),
+        'stripe_public_key' => $dotenv->pop('APP_STRIPE_PUBLIC_KEY'),
+        'stripe_webhook_secret' => $dotenv->pop('APP_STRIPE_WEBHOOK_SECRET'),
 
-        'flus_private_key' => getenv('FLUS_PRIVATE_KEY'),
+        'flus_private_key' => $dotenv->pop('APP_FLUS_PRIVATE_KEY'),
     ],
 
     'database' => [
@@ -25,17 +27,17 @@ return [
     ],
 
     'mailer' => [
-        'type' => getenv('APP_MAILER'),
-        'from' => getenv('APP_SMTP_FROM'),
+        'type' => $dotenv->pop('APP_MAILER'),
+        'from' => $dotenv->pop('APP_SMTP_FROM'),
         'smtp' => [
-            'domain' => getenv('APP_SMTP_DOMAIN'),
-            'host' => getenv('APP_SMTP_HOST'),
-            'port' => intval(getenv('APP_SMTP_PORT')),
-            'auth' => (bool)getenv('APP_SMTP_AUTH'),
-            'auth_type' => getenv('APP_SMTP_AUTH_TYPE'),
-            'username' => getenv('APP_SMTP_USERNAME'),
-            'password' => getenv('APP_SMTP_PASSWORD'),
-            'secure' => getenv('APP_SMTP_SECURE'),
+            'domain' => $dotenv->pop('APP_SMTP_DOMAIN'),
+            'host' => $dotenv->pop('APP_SMTP_HOST'),
+            'port' => intval($dotenv->pop('APP_SMTP_PORT')),
+            'auth' => (bool)$dotenv->pop('APP_SMTP_AUTH'),
+            'auth_type' => $dotenv->pop('APP_SMTP_AUTH_TYPE'),
+            'username' => $dotenv->pop('APP_SMTP_USERNAME'),
+            'password' => $dotenv->pop('APP_SMTP_PASSWORD'),
+            'secure' => $dotenv->pop('APP_SMTP_SECURE'),
         ],
     ],
 ];
