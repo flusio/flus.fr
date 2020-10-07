71e7a8e5a692c9444f5c483d004c5f1dd8440083
Marien Fressinaud <dev@marienfressinaud.fr>
2020-10-07 15:45:03 +0200

    Add account_id column to payments

--- src/schema.sql
+++ src/schema.sql
@@ -1,24 +1,3 @@
-CREATE TABLE payments (
-    id TEXT PRIMARY KEY NOT NULL,
-    created_at TEXT NOT NULL,
-    type TEXT NOT NULL,
-    invoice_number TEXT,
-    completed_at TEXT,
-    email TEXT NOT NULL,
-    amount INTEGER NOT NULL,
-    address_first_name TEXT NOT NULL,
-    address_last_name TEXT NOT NULL,
-    address_address1 TEXT NOT NULL,
-    address_postcode TEXT NOT NULL,
-    address_city TEXT NOT NULL,
-    address_country TEXT NOT NULL DEFAULT "FR",
-    payment_intent_id TEXT,
-    session_id TEXT,
-    username TEXT,
-    frequency TEXT,
-    company_vat_number TEXT
-);
-
 CREATE TABLE tokens (
     token TEXT PRIMARY KEY,
     created_at TEXT NOT NULL,
@@ -45,4 +24,32 @@ CREATE TABLE accounts (
     address_country TEXT,
 
     FOREIGN KEY (access_token) REFERENCES tokens(token) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE payments (
+    id TEXT PRIMARY KEY NOT NULL,
+    created_at TEXT NOT NULL,
+    completed_at TEXT,
+    type TEXT NOT NULL,
+
+    invoice_number TEXT,
+    email TEXT NOT NULL,
+    amount INTEGER NOT NULL,
+    username TEXT,
+    frequency TEXT,
+    company_vat_number TEXT,
+
+    address_first_name TEXT NOT NULL,
+    address_last_name TEXT NOT NULL,
+    address_address1 TEXT NOT NULL,
+    address_postcode TEXT NOT NULL,
+    address_city TEXT NOT NULL,
+    address_country TEXT NOT NULL DEFAULT "FR",
+
+    payment_intent_id TEXT,
+    session_id TEXT,
+
+    account_id TEXT,
+
+    FOREIGN KEY (account_id) REFERENCES accounts(id) ON UPDATE CASCADE ON DELETE SET NULL
 );
