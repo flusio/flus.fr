d33ecd2a4313b62e174b812cd9a7dcb69c8754d4
Marien Fressinaud <dev@marienfressinaud.fr>
2021-04-01 19:06:10 +0200

    tec: Remove cli/Invoices controller

--- src/Application.php
+++ src/Application.php
@@ -84,7 +84,6 @@ class Application
         $router->addRoute('cli', '/system/migrate', 'cli/System#migrate');
         $router->addRoute('cli', '/system/rollback', 'cli/System#rollback');
         $router->addRoute('cli', '/payments/complete', 'cli/Payments#complete');
-        $router->addRoute('cli', '/invoices/:id/email', 'cli/Invoices#sendPdf');
         $router->addRoute('cli', '/accounts', 'cli/Accounts#index');
         $router->addRoute('cli', '/accounts/create', 'cli/Accounts#create');
         $router->addRoute('cli', '/accounts/login-url', 'cli/Accounts#loginUrl');
