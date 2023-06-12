a7df2c1f6d1ee5f55e4af14f8cda6a8c758cf010
Marien Fressinaud <dev@marienfressinaud.fr>
2023-06-12 17:25:22 +0200

    tec: Remove call to dotenv in environment_test

--- configuration/environment_test.php
+++ configuration/environment_test.php
@@ -27,7 +27,7 @@ return [
 
         'support_email' => 'support@example.com',
 
-        'financial_goal' => intval($dotenv->pop('APP_FINANCIAL_GOAL', '36000')),
+        'financial_goal' => 36000,
 
         // used only in tests
         'number_of_datasets' => $number_of_datasets,
